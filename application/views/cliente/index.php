dcaaeb99f8dfba2577c8be9e6709f58c324b6330
colbert6 <colbersiho_6696@hotmail.com>
2016-07-14 14:05:52 -0500

    jose

---
+++ application/views/cliente/index.php
@@ -0,0 +1,92 @@
+
+<div class="row">
+    <div class="col-md-10">
+        <div class="box-body table-responsive">
+            <table  id="tab" class="table table-bordered table-striped">
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Dni</th>
+                        <th>Nombre</th>
+                        <th>Direccion</th>
+                        <th>Telefono</th>
+                        <th>Email</th>
+                        <th colspan="2">Acciones</th>
+                    </tr>
+                </thead>  
+            </table>
+            <div class="btn-group">
+                    <a class="btn btn-primary k-button" id="nuevo_modal" data-toggle="modal" data-target="#modal_form"><i class="fa  fa-plus"></i> Nuevo</a>
+            </div>
+        </div>
+    </div>   
+</div>
+
+<div class="modal fade" id="modal_form" tabindex="-1" role="dialog" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                <h4 class="modal-title"><i class="fa fa-users"></i> Formulario Cliente</h4>
+            </div>
+            <form role="form" action="" method="post">
+                <div class="modal-body">
+                    <div id="msg" class="form-group has-warning"></div>
+                    <div class="form-group">
+                        <label for="descripcion">Identificador</label>
+                        <input type="text" class="form-control" id="id" name="id" readonly="readonly" >
+                    </div>  
+                    <div class="form-group">
+                        <label for="descripcion">Dni</label>
+                        <input type="text" required class="form-control" id="dni" name="dni" placeholder="Ingrese DNI" onkeypress="return soloNumeros(event)" maxlength="8">
+                    </div>
+                    <div class="form-group">
+                        <label for="descripcion">Nombre y Apellido</label>
+                        <input type="text" required class="form-control" id="nombre" name="nombre" placeholder="Ingrese nombre y apellido" onkeypress="return soloLetras(event)">
+                    </div>
+                    <div class="form-group">
+                        <label for="descripcion">Direccion</label>
+                        <input type="text" required class="form-control" id="direccion" name="direccion" placeholder="Ingrese direccion" onkeypress="return soloLetras(event)">
+                    </div>
+                    <div class="form-group">
+                        <label for="descripcion">Telefono</label>
+                        <input type="text" required class="form-control" id="telefono" name="telefono" placeholder="Ingrese telefono" onkeypress="return soloNumeros(event)" maxlength="10">
+                    </div>
+                    <div class="form-group">
+                        <label for="descripcion">Email</label>
+                        <input type="text" required class="form-control" id="email" name="email" placeholder="Ingrese email" onkeypress="return soloLetras(event)">
+                    </div>
+                </div>
+                <div class="modal-footer clearfix">
+                    <button type="button" class="btn btn-danger" data-dismiss="modal"><i class="fa fa-times"></i> Cancelar</button>
+                    <button type="button" id='submit_form' class="btn btn-primary pull-left"><i class="fa fa-check"></i> Guardar</button>
+                </div>
+            </form>
+        </div><!-- /.modal-content -->
+    </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->                
+
+<div class="modal fade" id="modal_delete" tabindex="-1" role="dialog" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                <h4 class="modal-title"><i class="fa fa-exclamation-circle"></i> Alerta eliminar </h4>
+            </div>
+            <form role="form" action="" method="post">
+                <input type="hidden" id='id_dato_eliminar'></input>
+               
+                <div class="modal-body" >
+                    <div class="alert alert-danger alert-dismissable">
+                        <i class="fa fa-warning"></i>
+                        <h4>Estas seguro que desea eliminar el dato?</h4><h4 id="desc_dato_eliminar"></h4>
+                    </div>
+                </div>
+                <div class="modal-footer clearfix">
+                    <button type="button" class="btn btn-danger" data-dismiss="modal"><i class="fa fa-times"></i> Cancelar</button>
+                    <button type="button" id="delete_click" class="btn btn-primary pull-left"><i class="fa fa-check"></i> Aceptar</button>
+                </div>
+            </form>
+        </div><!-- /.modal-content -->
+    </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->                
